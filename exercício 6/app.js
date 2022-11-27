3b800983950265b915a598b3d7924d8513842b63
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2022-11-27 19:52:39 -0300

    pronto funcionado

--- exercício 6/app.js
+++ exercício 6/app.js
@@ -3,7 +3,7 @@ function CalcularSalario(){
     let dinheiro = document.getElementById("dinheiro").value
     let hora = document.getElementById("hora").value
 
-    let salario = (parseFloat(dinheiro) * parseFloat(hora))
+   let salario= parseFloat(dinheiro) * parseFloat(hora)
 
-    document.getElementById("salario").innerHTML = salariomensal
+    document.getElementById("resposta").innerHTML = salario
 }
