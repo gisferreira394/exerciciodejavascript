3f002c5a99057f92f0ce9b2ab7b86721c29ddaa7
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2022-12-06 19:08:31 -0300

    +/-

--- exercício9/app.js
+++ exercício9/app.js
@@ -1,10 +1,11 @@
 function Media(){
 
-let n1 = document.getElementById("n1").innerHTML.value
-let n2 = document.getElementById("n2").innerHTML.value
-let n3 = document.getElementById("n3").innerHTML.value
-
-let media= parseFloat(n1) + parseFloat(n2) + parseFloat(n3)/3
+    let n1= parseFloat (document.getElementById("n1").value )
+    let n2 = parseFloat (document.getElementById("n2").value)
+    let n3= parseFloat (document.getElementById("n3").value)
+    
+    let media= (n1+n2+n3)/3
+    
 
 if(media>=7){
 
