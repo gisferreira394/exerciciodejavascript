c8283b1bab340c8d23e6969181f03d57e8e4eb25
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2022-12-06 22:21:05 -0300

    pronto

--- exercício9/app.js
+++ exercício9/app.js
@@ -1,18 +1,18 @@
-function Media(){
+function media() {
 
-    let n1= parseFloat (document.getElementById("n1").value )
-    let n2 = parseFloat (document.getElementById("n2").value)
-    let n3= parseFloat (document.getElementById("n3").value)
-    
-    let media= (n1+n2+n3)/3
-    
+    let n1 = parseFloat(document.getElementById("n1").value)
+    let n2 = parseFloat(document.getElementById("n2").value)
+    let n3 = parseFloat(document.getElementById("n3").value)
+    let n4 = parseFloat(document.getElementById("n4").value)
 
-if(media>=7){
+    let media = (n1 + n2 + n3 + n4) / 4
 
-document.getElementById("resposta").innerHTML.value = "Aprovado parabéns"
-}
 
-else if(media>=5){
-
-    document.getElementById("resposta").innerHTML.value = " Recuperação "}
+    if (media <= 4) {
+        document.getElementById("resposta").innerHTML = "Reprovado" + media
+    }else if (media == 5) {
+        document.getElementById("resposta").innerHTML = " Recuperação" + media
+    }else if(media >= 6){
+        document.getElementById("resposta").innerHTML = " Aprovado parabéns" + media
+    }
 }
