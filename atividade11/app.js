3c8d6399f0ef083d5f54f01772790451f648fd13
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2022-12-13 20:38:47 -0300

    pronto

---
+++ atividade11/app.js
@@ -0,0 +1,12 @@
+function impar(){
+
+    let contador = 0
+ 
+    while (contador <= 100) {
+        if(contador % 2 == 1){
+        document.getElementById("resposta").innerHTML += contador +"    ,    "
+        }
+        contador++
+    }
+
+}
