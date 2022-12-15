205cdbdcad11edeb9c8ed5e9b1655c660f3d4f5f
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2022-12-15 11:37:25 -0300

    .

---
+++ atividade14/app.js
@@ -0,0 +1,12 @@
+function tabuada(){
+
+    let contador=0
+    let n1= document.getElementById("idade").value
+   document.getElementById("resposta").innerHTML=""
+
+    while(contador<=10){
+
+        document.getElementById("resposta").innerHTML +=n1 +"x"+ contador +"=" + contador*n1 + "<br>"
+        contador=contador+1
+    }
+}
